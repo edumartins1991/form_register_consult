63226adfec7ed08c80aa0a39d974bd08a249e3da
edumartins1991 <eduardo.martins1991@outlook.com>
2022-07-08 11:39:55 -0300

    adicionando_rotas

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
+
+app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Rotas
+app.get('/home', function(req, res){
+    res.render('home');
+});
+
+/*app.get('/', function(req, res){
+    res.render('');
+});*/
+
 app.listen(8080);
