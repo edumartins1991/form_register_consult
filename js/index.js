2bb69f7cd1c19b4babf282a1b087d8319f001972
edumartins1991 <eduardo.martins1991@outlook.com>
2022-07-08 14:12:01 -0300

    adicionando_body_parse

--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
-const handlebars = require("express-handlebars");
+const handlebars = require("express-handlebars");      
 
+app.use(express.static('public'));
 app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 //Rotas
 app.get('/home', function(req, res){
     res.render('home');
 });
 
-/*app.get('/', function(req, res){
-    res.render('');
-});*/
+app.post('/sucessform', function(req, res){
+    res.send("Nome " + req.body.name + "Sobrenome " + req.body.lastname + "email "+ req.body.email);
+});
 
 app.listen(8080);
